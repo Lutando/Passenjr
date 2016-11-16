05f55a9b12cfb092fa6883ba04475e8e9f0bde5b
Lutando Ngqakaza <lutando@ngqakaza.com>
2016-11-16 21:45:35 +0200

    Zoom Control
    removed zoom control

--- scripts/containers/MapContainer.js
+++ scripts/containers/MapContainer.js
@@ -1,11 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, Marker, Popup, TileLayer, ZoomControl } from 'react-leaflet';
 
 class MapContainer extends Component {
     render() {
         return (
-                <Map center={[-33.9231726,18.4217921]} zoom={13}>
+                <Map center={[-33.9231726,18.4217921]} zoom={13} zoomControl={false}>
                   <TileLayer
                     attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
                     url='http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
