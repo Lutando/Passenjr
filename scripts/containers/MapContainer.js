9fb7694cc76d95688401cdc71b044005f70b436e
Lutando Ngqakaza <lutando@ngqakaza.com>
2016-11-16 21:01:17 +0200

    React 15.4.0 Update
    updated hot module loader and packages

--- scripts/containers/MapContainer.js
+++ scripts/containers/MapContainer.js
@@ -6,15 +6,15 @@ class MapContainer extends Component {
     render() {
         return (
                 <Map center={[-33.9231726,18.4217921]} zoom={13}>
-                    <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        />
-        <Marker position={[-33.9231726,18.4217921]}>
-          <Popup>
-            <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
-          </Popup>
-        </Marker>
+                  <TileLayer
+                    attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+                    url='http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+                  />
+                  <Marker position={[-33.9231726,18.4217921]}>
+                    <Popup>
+                      <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
+                    </Popup>
+                  </Marker>
                 </Map>
         );
     }
