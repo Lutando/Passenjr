847e3b3f57a9b483be1590e25ee21874d0bff039
Lutando Ngqakaza <lutando@ngqakaza.com>
2016-11-17 09:57:20 +0200

    Viewport CSS

--- scripts/containers/App.js
+++ scripts/containers/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     }
     render() {
         return (    
-          <div>
+          <div className='full-height'>
             <MapContainer />
           </div>
         );
