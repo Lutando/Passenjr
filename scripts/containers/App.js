dc83102867cb0e016dfba6fd7d9a9e1192dcecfb
Lutando Ngqakaza <lutando@ngqakaza.com>
2016-12-01 07:20:57 +0200

    Journey Viewer start

--- scripts/containers/App.js
+++ scripts/containers/App.js
@@ -30,6 +30,7 @@ class App extends Component {
         return (    
           <div className='full-height'>
             <Nav />
+            <SideContainer />
             <MapContainer />
           </div>
         );
--- scripts/containers/SideContainer.js
+++ scripts/containers/SideContainer.js
@@ -1,10 +1,28 @@
 import React, { Component, PropTypes } from 'react';
+import { CardStack, Card } from 'react-cardstack';
 
 class SideContainer extends Component {
     render() {
         return (
             <div className='side-container'>
-            
+                <CardStack className='card-stack'
+    height={500}
+    width={400}
+    hoverOffset={25}>
+
+    <Card background='#162834' className='test'>
+        <h1>Number 1</h1>
+    </Card>
+
+    <Card background='#3f4f67'>
+        <h1>Number 2</h1>
+    </Card>
+
+    <Card background='#5f5f5f'>
+        <h1>Number 3</h1>
+    </Card>
+
+</CardStack>
             </div>
         );
     }
